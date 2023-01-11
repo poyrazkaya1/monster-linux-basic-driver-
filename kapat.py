21fe2ec44f0681f158d027a3663f75d22dbbb830
poyrazkaya1 <115424976+poyrazkaya1@users.noreply.github.com>
2023-01-11 20:29:24 +0300

    Add files via upload
    basit bir touchpad kapatma  kodu klavye kısayolları bir sonraki versiyonlarda güncellenecektir

---
+++ kapat.py
@@ -0,0 +1,11 @@
+
+import os 
+
+veri =input("touchpad açılsın mı kapatılsın mı ?")
+
+if veri == "kapat":
+    print("Touchpad kapatılıyor...")
+    os.system('xinput set-prop 14 "Device Enabled" 0')
+if veri == "aç":
+    print("touchpad açılıyor....")
+    os.system('xinput set-prop 14 "Device Enabled" 1')
